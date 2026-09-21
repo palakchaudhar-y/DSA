8bb31560e191b4165c37f82b0fe43dc6871f6627
palakchaudhar-y <palakchaudhary367@gmail.com>
2026-09-21 12:00:49 +0530

    Time: 68 ms (62.30%) | Memory: 78.2 MB (51.66%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,58 +1,36 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
 
-        int n = heights.length;
-
-        int[] pse = new int[n];
-        int[] nse = new int[n];
-
         Stack<Integer> st = new Stack<>();
+        int maxArea = 0;
 
-        // PSE
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i <= heights.length; i++) {
 
-            while (!st.isEmpty() && heights[st.peek()] >= heights[i]) {
-                st.pop();
+            int currHeight;
+
+            if (i == heights.length) {
+                currHeight = 0;
+            } else {
+                currHeight = heights[i];
             }
 
-            if (st.isEmpty()) {
-                pse[i] = -1;
-            } else {
-                pse[i] = st.peek();
+            while (!st.isEmpty() && heights[st.peek()] > currHeight) {
+
+                int element = st.pop();
+
+                int nsc = i;
+                int psc = st.isEmpty() ? -1 : st.peek();
+
+                int width = nsc - psc - 1;
+
+                int area = heights[element] * width;
+
+                maxArea = Math.max(maxArea, area);
             }
 
             st.push(i);
         }
 
-        st.clear();
-
-        // NSE
-        for (int i = n - 1; i >= 0; i--) {
-
-            while (!st.isEmpty() && heights[st.peek()] >= heights[i]) {
-                st.pop();
-            }
-
-            if (st.isEmpty()) {
-                nse[i] = n;
-            } else {
-                nse[i] = st.peek();
-            }
-
-            st.push(i);
-        }
-
-        int max = 0;
-
-        for (int i = 0; i < n; i++) {
-
-            int width = nse[i] - pse[i] - 1;
-
-            int area = heights[i] * width;
-
-            max = Math.max(max, area);
-        }
-
-        return max;
+        return maxArea;
     }
 }
