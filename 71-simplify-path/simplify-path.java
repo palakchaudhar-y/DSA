d86544ca5d279d3bb9f440599ff02f1b98b6cae0
palakchaudhar-y <palakchaudhary367@gmail.com>
2026-09-24 01:49:28 +0530

    Time: 5 ms (51.82%) | Memory: 45.2 MB (27.87%) - LeetSync

---
+++ 71-simplify-path/simplify-path.java
@@ -0,0 +1,26 @@
+class Solution {
+    public String simplifyPath(String path) {
+        Stack<String> st=new Stack<>();
+        String[] parts=path.split("/");
+        for(int i=0;i<parts.length;i++){
+            String part=parts[i];
+            if(part.equals("") || part.equals(".")){
+                continue;
+            }else if(part.equals("..")){
+                if(!st.isEmpty()){
+                st.pop();}
+            }
+            else{
+                st.push(part);
+            }
+        }
+        StringBuilder sb=new StringBuilder();
+        while(!st.isEmpty()){
+            sb.insert(0,"/"+st.pop());
+        }
+        if(sb.length()==0){
+        return "/";
+    }
+        return sb.toString();
+    }
+}
